1d58b6b2482bbf4c9a05a3065d354468bf014e09
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-07-31 10:41:56 +0200

    [add] Investment app

---
+++ src/components/Results.jsx
@@ -0,0 +1,7 @@
+import { calculateInvestmentResults } from "../util/investment";
+
+export default function Results({ input }) {
+	calculateInvestmentResults(input);
+
+	return <p>Results...</p>;
+}
